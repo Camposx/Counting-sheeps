cc76e972660877073b633ae7e7236cd2a5e1b427
Camposx <alejandrocamposgon@gmail.com>
2024-10-30 12:17:08 +0100

    Add test for data type

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 function countSheeps(list){
     let sheeps = 0;
     let wolves = 0;
-
+    
     for(let i = 0; i < list.length; i++){
         if (list[i] == true){
             sheeps++;
-        }
+        };
         if(list[i] == false){
             wolves++;
-        }  
+        };
+        if(list[i] != false && list[i] != true){
+            return console.log("Error!!! List only accepts boolean values") ;
+        }
     };
+
     if(sheeps){
         console.log(`There are ${sheeps} sheep in total`)
     }
@@ -18,12 +22,11 @@ function countSheeps(list){
     }
     if(!sheeps && !wolves){
         console.log("There are neither sheep or wolves")
-    }
+    }  
+    
 }
 
-let list = [true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, 
+let list = [true,  true,  true,  false, true, true , true,  false, true,  false, 
     true,  false, false, true , true,  true,  true,  true , false, false, true,  true];
-countSheeps(list);
 
-let list2 = [false, false, false];
-countSheeps(list2);
+countSheeps(list);
